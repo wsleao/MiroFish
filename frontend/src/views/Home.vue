bec9269c7cf486771e7d9b5c2e4a464030f6870a
wsleao <wsleao@gmail.com>
2026-06-03 20:03:01 -0300

    confirma

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -44,7 +44,7 @@
         <div class="hero-right">
           <!-- Logo 区域 -->
           <div class="logo-container">
-            <img src="../assets/logo/MiroFish_logo_left.jpeg" alt="MiroFish Logo" class="hero-logo" />
+            <img src="../assets/logo/Norvix sem fundo.png" alt="MiroFish Logo" class="hero-logo" />
           </div>
           
           <button class="scroll-down-btn" @click="scrollToBottom">
