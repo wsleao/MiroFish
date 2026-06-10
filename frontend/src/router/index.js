094fb7473d965cc580d107c35ba6471833fefea7
wsleao <wsleao@gmail.com>
2026-06-09 22:51:38 -0300

    feat: protect NSC routes with SaaS login

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,52 +1,76 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
+import LoginView from '../views/LoginView.vue'
 import Process from '../views/MainView.vue'
 import SimulationView from '../views/SimulationView.vue'
 import SimulationRunView from '../views/SimulationRunView.vue'
 import ReportView from '../views/ReportView.vue'
 import InteractionView from '../views/InteractionView.vue'
+import { isAuthenticated } from '../store/auth'
 
 const routes = [
   {
+    path: '/login',
+    name: 'Login',
+    component: LoginView,
+    meta: { public: true }
+  },
+  {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: { requiresAuth: true }
   },
   {
     path: '/process/:projectId',
     name: 'Process',
     component: Process,
-    props: true
+    props: true,
+    meta: { requiresAuth: true }
   },
   {
     path: '/simulation/:simulationId',
     name: 'Simulation',
     component: SimulationView,
-    props: true
+    props: true,
+    meta: { requiresAuth: true }
   },
   {
     path: '/simulation/:simulationId/start',
     name: 'SimulationRun',
     component: SimulationRunView,
-    props: true
+    props: true,
+    meta: { requiresAuth: true }
   },
   {
     path: '/report/:reportId',
     name: 'Report',
     component: ReportView,
-    props: true
+    props: true,
+    meta: { requiresAuth: true }
   },
   {
     path: '/interaction/:reportId',
     name: 'Interaction',
     component: InteractionView,
-    props: true
+    props: true,
+    meta: { requiresAuth: true }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+})
+
+router.beforeEach((to) => {
+  if (to.meta.requiresAuth && !isAuthenticated()) {
+    return { name: 'Login' }
+  }
+  if (to.name === 'Login' && isAuthenticated()) {
+    return { name: 'Home' }
+  }
+  return true
 })
 
 export default router
